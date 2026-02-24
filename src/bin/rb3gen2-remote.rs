51b30a38a840065464f1bdc0b1c145df6ea4f2c6
Daniel Thompson <daniel@redfelineninja.org.uk>
2026-02-24 11:53:16 +0000

    Remote both applications.
    Ignore what git diff says. There are no changes.

--- src/bin/rb3gen2-remote.rs
+++ src/bin/rb3gen2-remote.rs
@@ -4,9 +4,9 @@ use std::{
 };
 
 use clap::{Parser, Subcommand, ValueEnum};
-use expectrl::Error;
+use expectrl::{Error, repl::ReplSession, session::OsSession};
 
-use dutlib::{CommandExecutor, native::NativeExecutor, plans, tests};
+use dutlib::{CommandExecutor, dut::DeviceUnderTest, plans, tests};
 
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
@@ -26,8 +26,14 @@ struct Cli {
 
 #[derive(Debug, Subcommand)]
 enum Commands {
+    /// Reboot the target (using the least aggressive means necessary)
+    Reboot(RebootCli),
+
     /// Run a simple smoke test
     SmokeTest(TestCli),
+
+    /// Boot cycling test (subset of smoke test) to estimate boot reliability
+    BootCycle(BootCycleCli),
 
     /// Functional testing (inc. data integrity)
     FunctionalTest(TestCli),
@@ -37,6 +43,17 @@ enum Commands {
 
     /// Run all tests that do not require the board to be rebooted.
     AllTests(TestCli),
+}
+
+#[derive(Debug, Parser)]
+pub struct RebootCli {}
+
+fn reboot(_args: RebootCli) -> Result<(), Error> {
+    let mut board = DeviceUnderTest::new();
+    let mut console = board.console()?;
+    console.cmd("reboot")?;
+
+    Ok(())
 }
 
 #[derive(Debug, Parser)]
@@ -61,7 +78,7 @@ pub enum TestPlan {
     LatencyTest,
 }
 
-type TestPlanRunner = fn(&mut NativeExecutor, &str, &str) -> Result<u32, Error>;
+type TestPlanRunner = fn(&mut ReplSession<OsSession>, &str, &str) -> Result<u32, Error>;
 
 impl TestPlan {
     fn name(&self) -> &'static str {
@@ -81,20 +98,89 @@ impl TestPlan {
     }
 }
 
+#[derive(Debug, Parser)]
+pub struct BootCycleCli {
+    /// Name of the network driver module to be loaded
+    #[arg(short, long, default_value = "dwmac_tc956x")]
+    module: String,
+
+    /// Name of the device (as it appears in `ip addr`)
+    #[arg(short, long, default_value = "enP1p5s0f1")]
+    name: String,
+
+    /// IP address (or name) of a machine running `iperf3 -s`
+    #[arg(short, long, default_value = "192.168.10.2")]
+    ipaddr: String,
+
+    /// Number of boot cycles to perform
+    #[arg(short, long, default_value_t = 100)]
+    cycles: u32,
+
+    /// Set the number of times to run the test plan per boot
+    #[arg(long, default_value_t = 1)]
+    cycles_per_boot: u32,
+
+    /// Choose which test plan to cycle through
+    #[arg(short, long, default_value = "smoke-test")]
+    plan: TestPlan,
+}
+
+fn boot_cycle(args: BootCycleCli) -> Result<(), Error> {
+    let mut board = DeviceUnderTest::new();
+
+    let mut good = 0;
+    let mut bad = 0;
+
+    let mut remaining_this_boot = 0;
+    let mut console = board.console()?;
+
+    for cycle in 0..args.cycles {
+        if remaining_this_boot <= 1 {
+            let _ = board.reboot(console);
+            console = board.console_with_module(&args.module)?;
+            let _ = tests::uname(&mut console).inspect_err(|e| log::error!("{e}"));
+            remaining_this_boot = args.cycles_per_boot;
+        } else {
+            remaining_this_boot -= 1;
+        }
+
+        match args.plan.runner()(&mut console, &args.name, &args.ipaddr) {
+            Ok(0) => good += 1,
+            Ok(n) => {
+                bad += 1;
+                log::error!("{n} tested failed");
+            }
+            Err(err) => {
+                bad += 1;
+                log::error!("{err}");
+                log::info!("{:?}", console.try_read_to_string());
+            }
+        };
+
+        log::info!(
+            "Boot success is {:4.1}% after {} iterations",
+            (100 * good) as f64 / (good + bad) as f64,
+            cycle + 1
+        );
+    }
+
+    Ok(())
+}
+
 fn run_test<T>(args: TestCli, test_plan: T) -> Result<(), Error>
 where
-    T: FnOnce(&mut NativeExecutor, &str, &str) -> Result<u32, Error>,
+    T: FnOnce(&mut ReplSession<OsSession>, &str, &str) -> Result<u32, Error>,
 {
-    let mut shell = NativeExecutor::new();
-    tests::uname(&mut shell)?;
-    shell.load_module(&args.module)?;
+    let mut board = DeviceUnderTest::new();
+    let mut console = board.console_with_module(&args.module)?;
+    tests::uname(&mut console)?;
 
-    match test_plan(&mut shell, &args.name, &args.ipaddr) {
+    match test_plan(&mut console, &args.name, &args.ipaddr) {
         Ok(0) => Ok(()),
         Ok(n) => Err(io::Error::other(format!("{n} failures reported")).into()),
         Err(e) => {
-            log::error!("Test plan did not complete");
-            log::info!("Debug info: {:?}", shell.try_read_to_string());
+            log::error!("Test plan failed to complete due to internal error");
+            log::info!("{:?}", console.try_read_to_string());
             Err(e)
         }
     }
@@ -107,21 +193,31 @@ fn all_tests(args: TestCli) -> Result<(), Error> {
         TestPlan::LatencyTest,
     ];
 
-    let mut shell = NativeExecutor::new();
-    tests::uname(&mut shell)?;
-    shell.load_module(&args.module)?;
+    let mut board = DeviceUnderTest::new();
+    let mut console = board.console_with_module(&args.module)?;
+    tests::uname(&mut console)?;
 
     let mut failures = 0;
 
     for (plan, name) in tests.iter().map(|p| (p.runner(), p.name())) {
-        let result = plan(&mut shell, &args.name, &args.ipaddr)?;
+        let result = plan(&mut console, &args.name, &args.ipaddr);
         match result {
-            0 => {
+            Ok(0) => {
                 log::info!("{name} completed successfully");
             }
-            n => {
+            Ok(n) => {
                 log::info!("{name} reported {n} failures");
                 failures += n;
+            }
+            Err(err) => {
+                log::error!("{name} failed to complete due to an internal error ({err})");
+                log::info!("{:?}", console.try_read_to_string());
+                failures += 1;
+
+                // Now let's try to recover the system so we can run the next
+                // set of tests
+                board.crashed(console.into_session());
+                console = board.console_with_module(&args.module)?;
             }
         }
     }
@@ -141,7 +237,7 @@ fn app() -> Result<(), Error> {
         "error",
         "warn",
         "info,dutlib=warn",
-        "info,dutlib::native=warn",
+        "info,dutlib::dut=warn",
         "info", // default
         "debug",
         "trace",
@@ -155,7 +251,9 @@ fn app() -> Result<(), Error> {
     env_logger::Builder::from_env(env).init();
 
     match cli.command {
+        Commands::Reboot(args) => reboot(args),
         Commands::SmokeTest(args) => run_test(args, plans::smoke_test),
+        Commands::BootCycle(args) => boot_cycle(args),
         Commands::FunctionalTest(args) => run_test(args, plans::functional_test),
         Commands::LatencyTest(args) => run_test(args, plans::latency_test),
         Commands::AllTests(args) => all_tests(args),
