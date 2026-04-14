d3a62dbcfc8f7bbaf9f61733e757fa791f7073ad
Daniel Thompson <daniel@riscstar.com>
2026-04-14 10:12:27 +0100

    tests: verify_log_messages: Suppress AER errors reported from tc956x-pci
    This haven't been observed before when running the test suite but that's
    likely just a matter of timing. Even within the log we have where
    tc956x-pci was tagged if then identical messages tagged pcieport were
    more common.

--- src/tests.rs
+++ src/tests.rs
@@ -368,7 +368,7 @@ pub fn verify_log_messages(
             // [            ] pcieport 0001:01:00.0: AER:   Error of this Agent is reported first
             if !lower.contains("warn")
                 && !lower.contains("bug")
-                && lower.contains("pcieport")
+                && (lower.contains("pcieport") || lower.contains("tc956x-pci"))
                 && (lower.contains("correctable")
                     || lower.contains("error status/mask=")
                     || lower.contains("error of this agent reported first"))
